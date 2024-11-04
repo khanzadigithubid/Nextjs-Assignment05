6a9223c7bb2858b948ca1b5e0b381e4544549669
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-11-04 23:27:08 +0500

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="h-[130px] md:h-[100px] bg-[#787256] flex items-center justify-between px-4 md:px-6">
+    <div className="h-[130px] bg-[#787256] flex items-center justify-between px-4 md:px-6">
       <h1 className="font-bodoni text-[60px] md:text-[75px] text-white">MANZZARI</h1>
       </div>
   );
