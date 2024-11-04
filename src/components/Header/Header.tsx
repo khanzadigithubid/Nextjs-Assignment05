9ae343039532ad613dba96ff9e98d7b57faecf1c
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-11-04 23:15:53 +0500

    This is Nextjs Assignment05 Figma Design

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="h-[130px] md:h-[100px] bg-[#787256] flex items-center justify-between px-4 md:px-6">
+      <h1 className="font-bodoni text-[60px] md:text-[75px] text-white">MANZZARI</h1>
+      </div>
+  );
+};
+
+export default Header;
